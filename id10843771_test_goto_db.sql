50265792f5a72f525ec56744930f15013ea84741
Alexander <sever6131@ya.ru>
2019-09-13 11:00:39 +0600

    Add files via upload

---
+++ id10843771_test_goto_db.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost:3306
+-- Время создания: Сен 13 2019 г., 04:59
+-- Версия сервера: 10.3.16-MariaDB
+-- Версия PHP: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `id10843771_test_goto_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `short`
+--
+
+CREATE TABLE `short` (
+  `id` int(11) NOT NULL,
+  `url` text COLLATE utf8_unicode_ci NOT NULL,
+  `short_key` varchar(5) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `short`
+--
+
+INSERT INTO `short` (`id`, `url`, `short_key`) VALUES
+(37, 'https://github.com/a-gellert/short_url', 'afq0r');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `short`
+--
+ALTER TABLE `short`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `short_key` (`short_key`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `short`
+--
+ALTER TABLE `short`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
